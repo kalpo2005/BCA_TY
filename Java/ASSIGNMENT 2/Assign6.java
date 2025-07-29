09d48604f27a376b5c90a373042eef0f2bc21214
KALPESH BAVALIYA <kalpesh2005@gmail.com>
2025-07-29 10:47:17 +0530

    java assignment Date : 29/07/2025

--- Java/ASSIGNMENT 2/Assign6.java
+++ Java/ASSIGNMENT 2/Assign6.java
@@ -1,28 +1,69 @@
 import java.util.Scanner;
-class Assign6{
 
+class Shopping{
+		
+	int itemCode,itemQnt,itemPrice,total;
+	String itemName;
+	
+	void input(	Scanner sc){
+
+		System.out.print("Enter a item code: ");
+		itemCode = sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("Enter a item name:");
+		itemName = sc.nextLine();
+		
+		System.out.print("Enter a item Quentity : ");
+		itemQnt = sc.nextInt();
+		
+		System.out.print("Enter a item Price : ");
+		itemPrice = sc.nextInt();
+		
+		total = itemPrice * itemQnt;	
+	}
+	
+	void display(){
+		System.out.print(itemCode + "     ");
+		System.out.print(itemName + "     ");
+		System.out.print(itemPrice + "     ");
+		System.out.print(itemQnt + "     ");
+		System.out.print(total + "     ");
+	
+	}
+}
+
+class Assign6{
     public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int i = 0,toalAmount = 0;
+		Shopping[] kal = new Shopping[5];
+		char choice;
+	
+		do{
+			
+			kal[i++].input(sc);
+			sc.nextLine();
+			System.out.println("\n Do you continue to add items : y/n ......");
+			choice = sc.next().charAt(0);                     
+			
+		
+		}while(choice == 'Y' || choice == 'y');
+		
+		System.out.println("\n \n Your bill is a : \n");
+		System.out.println("item code     item name    item price    item qntity     total");
+		System.out.println("---------------------------------------------------------------------");
+		
+		for(int k = 0 ; k < i ; k++){
+			
+			obj[k].display();
+			toalAmount += obj[i].total;
+		}
+		
+		// Printing a gross total 
+		System.out.println("A total paybale ammount is a :                               "+ toalAmount);
 
     }
 
 }
 
-class Shopping{
-		
-	int itemCode,itemQnt,itemPrice;
-	String itemName;
-	
-	void input(){
-		Scanner sc = new Scanner();
-		System.out.print("Enter a item code: ");
-		itemCode = sc.nextLine();
-		
-		System.out.print("Enter a item name:");
-		itemCode = sc.nextLine();
-		
-		System.out.print"Enter a item Quenty : ");
-		itemCode = sc.nextLine();
-		
-		
-	}
-}
--- Java/ASSIGNMENT 2/Assign7.java
+++ Java/ASSIGNMENT 2/Assign7.java
@@ -1,8 +1,60 @@
 import java.util.Scanner;
-class Assign7{
 
+class Employee{
+		
+	int no,age,salary;
+	String name;
+	
+	void input(	Scanner sc){
+
+		System.out.print("Enter an employee id: ");
+		no = sc.nextInt();
+		
+		sc.nextLine();
+		System.out.print("Enter an employee name :");
+		name = sc.nextLine();
+		
+		System.out.print("Enter an employee Age : ");
+		age = sc.nextInt();
+		
+		System.out.print("Enter a employee salary : ");
+		salary = sc.nextInt();
+			
+	}
+	
+	void display(){
+		System.out.print(no + "     ");
+		System.out.print(name + "     ");
+		System.out.print(age + "     ");
+		System.out.print(salary + "     ");
+	
+	}
+}
+
+class Assign6{
     public static void main(String[] args){
-
+		Scanner sc = new Scanner(System.in);
+		Shopping[] obj = new Shopping[100];
+		int i = 0;
+	
+		// Enter a data in the of three number
+		System.out.prinln("Enter a three employee Detail : \n ")
+		do{
+			Employee kal = new Employee();
+			obj[i++] = kal;
+			kal.input(sc);
+			sc.nextLine();                     
+			
+		
+		}while(i<=3);
+		
+		System.out.println("\n \n Employee Detail is a  : \n");
+		System.out.println("no     employee name    employee age    employee slary");
+		System.out.println("---------------------------------------------------------------------");
+		
+		for(int k = 0 ; k < i ; k++){
+			obj[i].display();
+		}
     }
 
 }
